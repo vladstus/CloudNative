4a35662383056b743e975f2664237b3bb288df02
vladstus <vlad.stus@gmail.com>
2023-06-13 23:30:33 +0300

    some fixes for pipeline

--- catalog-service/src/test/java/com/vladstus/catalogservice/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/vladstus/catalogservice/CatalogServiceApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
 )
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
     @Autowired
