4ed45f69773593dc5f2a0912fd6158ec67f833e0
vladstus <vlad.stus@gmail.com>
2023-06-13 19:49:57 +0300

    Adding config subrepo

---
+++ catalog-service/src/main/java/com/vladstus/catalogservice/demo/BookDataLoader.java
@@ -0,0 +1,28 @@
+package com.vladstus.catalogservice.demo;
+
+import com.vladstus.catalogservice.domain.Book;
+import com.vladstus.catalogservice.domain.BookRepository;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("testdata")
+public class BookDataLoader {
+    private final BookRepository bookRepository;
+
+    public BookDataLoader(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void loadBookTestData() {
+        var book1 =
+                new Book("1234567891", "Northern Lights", "Lyra Sivlerstar", 9.90);
+        var book2 = new Book("1234567892", "Polar Journey", "Iorek Polarson", 12.90);
+
+        bookRepository.save(book1);
+        bookRepository.save(book2);
+    }
+}
