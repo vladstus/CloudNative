d7f45166dbeef8f2f992a33e5848bfa919913e77
vladstus <vlad.stus@gmail.com>
2023-06-13 18:54:35 +0300

    Added example of configuration properties usage 2

---
+++ catalog-service/src/main/java/com/vladstus/catalogservice/HomeController.java
@@ -0,0 +1,22 @@
+package com.vladstus.catalogservice;
+
+import com.vladstus.catalogservice.config.PolarProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HomeController {
+
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return polarProperties.getGreeting();
+    }
+}
