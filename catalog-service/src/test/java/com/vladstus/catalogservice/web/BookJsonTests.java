3bbf9c03ae43d563e689b35f0351c1b1ede533d9
vladstus <vlad.stus@gmail.com>
2023-06-14 00:14:13 +0300

    some other changes to tests

--- catalog-service/src/test/java/com/vladstus/catalogservice/web/BookJsonTests.java
+++ catalog-service/src/test/java/com/vladstus/catalogservice/web/BookJsonTests.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @JsonTest
+@ActiveProfiles("integration")
 public class BookJsonTests {
     @Autowired
     private JacksonTester<Book> json;
