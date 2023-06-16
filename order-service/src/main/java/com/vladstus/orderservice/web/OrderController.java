e47fd3617aca9c4cece5cf17756d5f2ef6d83a41
vladstus <vlad.stus@gmail.com>
2023-06-16 22:48:05 +0300

    Reactive Rest Controller for order-service

---
+++ order-service/src/main/java/com/vladstus/orderservice/web/OrderController.java
@@ -0,0 +1,31 @@
+package com.vladstus.orderservice.web;
+
+import com.vladstus.orderservice.domain.Order;
+import com.vladstus.orderservice.domain.OrderService;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public Flux<Order> getAllOrders() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(@RequestBody @Valid OrderRequest orderRequest) {
+        return orderService.submitOrder(orderRequest.isbn(), orderRequest.quantity());
+    }
+
+
+}
