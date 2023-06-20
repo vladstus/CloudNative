024be415d241adcc45abf9a5042c7b1ebf512f4e
vladstus <vlad.stus@gmail.com>
2023-06-20 10:09:53 +0300

    created dispatcher service with Spring Cloud Function dependencies

--- Notes.md
+++ Notes.md
@@ -1,6 +1,6 @@
 # Notes
 Book: Spring Native
-Last page: 647
+Last page: 654
 
 # TODO
 
---
+++ dispatcher-service/src/test/java/com/vladstsu/dispatcherservice/DispatcherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vladstsu.dispatcherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
