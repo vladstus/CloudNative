7aa61539eb615716733fdeac9413843b3638814b
vladstus <vlad.stus@gmail.com>
2023-06-26 17:23:14 +0300

    optimized some imports

--- edge-service/src/main/java/com/vladstus/edgeservice/config/SecurityConfig.java
+++ edge-service/src/main/java/com/vladstus/edgeservice/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
